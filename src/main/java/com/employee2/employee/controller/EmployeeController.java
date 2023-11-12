b3f45858c606e6fa75fc8b9b0e9f151ecb5e8f2e
Надежда <nadin94403@gmail.com>
2023-11-12 21:54:21 +0300

    На проверку, ДЗ по библиотекам

--- src/main/java/com/employee2/employee/controller/EmployeeController.java
+++ src/main/java/com/employee2/employee/controller/EmployeeController.java
@@ -2,22 +2,22 @@ package com.employee2.employee.controller;
 
 import com.employee2.employee.model.Employee;
 import com.employee2.employee.service.EmployeeServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
 @RequestMapping( "/employee")
+
 public class EmployeeController {
     private final EmployeeServiceImpl service;
 
     public EmployeeController(EmployeeServiceImpl service) {
         this.service = service;
     }
+
 @GetMapping(path = "/add")
     public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName, @RequestParam int department,@RequestParam int salary) {
         return service.add(firstName, lastName, department,salary);
@@ -34,4 +34,5 @@ public class EmployeeController {
     public Collection <Employee> getCollection () {
         return service.getEmployees();
     }
+
 }
